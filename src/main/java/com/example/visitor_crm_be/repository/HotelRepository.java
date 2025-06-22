7f11c16fb8ce9c3c21b945256430bdf0316cfa8b
gokturksigirtmac <s.gokturk@outlook.com>
2025-06-22 20:47:22 +0300

    Crone job for trial account

--- src/main/java/com/example/visitor_crm_be/repository/HotelRepository.java
+++ src/main/java/com/example/visitor_crm_be/repository/HotelRepository.java
@@ -3,8 +3,11 @@ package com.example.visitor_crm_be.repository;
 import com.example.visitor_crm_be.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
     Optional<Hotel> findByHotelName(String hotelName);
+    List<Hotel> findAllByTrialTrueAndSuspendedFalseAndTrialExpiresAtBefore(LocalDateTime trial_expires_at);
 }
